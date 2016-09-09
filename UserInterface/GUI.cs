719ee372b211853e1f3809ae424366937f880a67
Daniel-Lee <706321263@qq.com>
2016-09-09 17:17:02 +0800

    初步测试生成曲线

--- UserInterface/GUI.cs
+++ UserInterface/GUI.cs
@@ -26,49 +26,54 @@ namespace GUI
 
             try
             {
-                chart1.Series[0].Points.DataBindXY(listX, listY);
+                //chart1.Series[0].Points.DataBindXY(listX, listY);
                 chart1.Series[0].Points.DataBindY(listY);
 
                 //chart1.Series[0].MarkerColor = Color.Green;               //unknown
                 // 设置为折线显示
                 chart1.Series[0].ChartType = SeriesChartType.Spline;
-                chart1.Series[0].MarkerSize = 5;    
-                chart1.Series[0].Color = Color.Orange;
-                chart1.Series[0].BorderWidth = 2;
-                chart1.Series[0].IsValueShownAsLabel = false;
+                //chart1.Series[0].MarkerSize = 5;    
+                //chart1.Series[0].Color = Color.Orange;
+                //chart1.Series[0].BorderWidth = 2;
+                //chart1.Series[0].IsValueShownAsLabel = false;
 
                 // 可以点击，左右查看
                 // 显示竖线 红色
-                chart1.ChartAreas[0].CursorX.IsUserEnabled = true;       
-                chart1.ChartAreas[0].CursorX.AutoScroll = true;
+                //chart1.ChartAreas[0].CursorX.IsUserEnabled = true;       
+                //chart1.ChartAreas[0].CursorX.AutoScroll = true;
+                // 设置能左右查看未显示的数据
                 chart1.ChartAreas[0].CursorX.IsUserSelectionEnabled = true;
-                chart1.ChartAreas[0].AxisX.ScaleView.Zoomable = true;
+                //chart1.ChartAreas[0].AxisX.ScaleView.Zoomable = true;
+                // 设置滚动条位置在图表外
                 chart1.ChartAreas[0].AxisX.ScrollBar.IsPositionedInside = false;
+                // 设置滚动条大小
                 chart1.ChartAreas[0].AxisX.ScrollBar.Size = 15;
-                chart1.ChartAreas[0].AxisX.ScrollBar.ButtonStyle = ScrollBarButtonStyles.All;
-                chart1.ChartAreas[0].AxisX.ScrollBar.ButtonColor = Color.SkyBlue;
-                chart1.ChartAreas[0].AxisX.ScaleView.SmallScrollSize = double.NaN;
-                chart1.ChartAreas[0].AxisX.ScaleView.SmallScrollMinSize = 1;
-                chart1.ChartAreas[0].AxisX.Interval = 10;
+                //chart1.ChartAreas[0].AxisX.ScrollBar.ButtonStyle = ScrollBarButtonStyles.All;
+                //chart1.ChartAreas[0].AxisX.ScrollBar.ButtonColor = Color.SkyBlue;
+                //chart1.ChartAreas[0].AxisX.ScaleView.SmallScrollSize = double.NaN;
+                //chart1.ChartAreas[0].AxisX.ScaleView.SmallScrollMinSize = 1;
+                // 设置x轴间隔
+                chart1.ChartAreas[0].AxisX.Interval = listX.Average() / 10;
+                //设置竖直标线
                 chart1.ChartAreas[0].AxisX.MajorGrid.Enabled = false;
-                chart1.ChartAreas[0].AxisX.Title = "x title";
-                chart1.ChartAreas[0].AxisY.Title = "y title";
-                double max = listY[0];
-                double min = listY[0];
-                foreach (var yValue in listY)
-                {
-                    if (max < yValue)
-                    {
-                        max = yValue;
-                    }
-                    if (min > yValue)
-                    {
-                        min = yValue;
-                    }
-                }
-                chart1.ChartAreas[0].AxisY.Maximum = max;
-                chart1.ChartAreas[0].AxisY.Minimum = min;
-                chart1.ChartAreas[0].AxisY.Interval = (max - min) / 10;
+                //chart1.ChartAreas[0].AxisX.Title = "x title";
+                //chart1.ChartAreas[0].AxisY.Title = "y title";
+                //double max = listY[0];
+                //double min = listY[0];
+                //foreach (var yValue in listY)
+                //{
+                //    if (max < yValue)
+                //    {
+                //        max = yValue;
+                //    }
+                //    if (min > yValue)
+                //    {
+                //        min = yValue;
+                //    }
+                //}
+                chart1.ChartAreas[0].AxisY.Maximum = listY.Average() * 4;
+                chart1.ChartAreas[0].AxisY.Minimum = 0;
+                chart1.ChartAreas[0].AxisY.Interval = listY.Average() / 2;
                 //绑定数据源
                 chart1.DataBind();
             }
